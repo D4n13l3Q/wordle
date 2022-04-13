6e9542df25a9f2f57c67c03a0ca043066bd6fb0e
D4n13l3Q <daniele.quasimodo@edu.itspiemonte.it>
2022-04-13 15:36:56 +0200

    Prototipo

--- index.mjs
+++ index.mjs
@@ -1,91 +1,109 @@
-/**
- * Implementare WORDLE. [https://www.nytimes.com/games/wordle/index.html]
- *   Ogni tentativo deve avere 4 lettere valide.
- *
- *   Il colore della lettera deve cambiare secondo le seguenti regole:
- *
- *   VERDE: la lettera è contenuta nella soluzione nella posizione giusta
- *   GIALLO: la lettera è contenuta nella soluzione ma NON nella posizione giusta
- *   GRIGIO: la lettera non è contenuta nella soluzione
- *
- * */
+import readline from "readline";
 
-import chalk from 'chalk';
-import readline from 'readline';
+import chalk from "chalk";
 
-// https://nodejs.org/api/readline.html#readlinecreateinterfaceoptions
+
+
+const secret = "rebus";
+
+
+
+
+// Funzioni
+
+const check = function(input, secret) {
+    
+    let guessed = true;
+    let output = [];
+    
+    for (let i in secret) {
+        
+        if (secret[i] === input[i]) {
+        
+            output.push(chalk.green(input[i]));
+        
+        } else {
+         
+            if (secret.includes(input[i])) {
+                
+                output.push(chalk.yellow(input[i]));
+                
+            } else {
+                
+                output.push(chalk.grey(input[i]));
+                
+            }
+            
+            guessed = false;
+            
+        }
+                
+    }
+    
+    return {
+        output : output.join("-"),
+        guessed : guessed
+    };
+    
+}
+
+
+
+
+
+
+const maxAttempts = 6;
+
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout,
-})
+    output: process.stdout
+});
 
-let attempt = 0 // contatore dei tentativi
-let maxGameAttempt = 6; // abbiamo 6 tentativi per risolvere una parola
-let wordGameLength = 4; // la parola deve esser lunga al max 4
-
-// Soluzione statica a fine didattico.
-const solution = 'CODE';
-
-// funzione di verifica
-const wordChecker = function (word, solution) {
-    let success = [];
-    let result = [];
-
-    // ATTENZIONE ALLE PAROLE maiuscole/minuscole.
-
-    // controllo lettera per lettera in parola
-    for (let index in word) {
-        // VERDE: la lettera è contenuta nella parola nella posizione giusta
-        // GIALLO: la lettera è contenuta nella parola ma NON nella posizione giusta
-        // GRIGIO: la lettera non è contenuta nella parola
-    }
-
-    // ritorno un risultato composto dalle lettere colorate e da un success booleano
-    // per fare uscire dal gioco se l'utente indovina la parola
-    return {
-        'data': '',
-        'success': true
-    }
-}
-
-// funzione di Gioco
-const game = function (attempt, max) {
-    // https://nodejs.org/api/readline.html#rlquestionquery-options-callback
-    rl.question(chalk.blue(`Inserisci una parola di ${wordGameLength} caratteri: `), function (answer) {
-
-        // usiamo una safe word per uscire dal ciclo
-        if (answer === 'exit') {
-            return rl.close();
+const game = function(attempts, prompt = "Inserisci una parola: ") {
+    
+    if (attempts <= 0) return rl.close(); // Se i tentativi rimasti sono meno di zero (zero è l'ultimo tetnativo valido) esco chiudendo readline.
+    
+    rl.question(prompt, (userInput) => {
+        
+        if (userInput.length !== secret.length || !(/^[a-zA-Z]+$/.test(userInput))) { // Se "userInput" è più o meno lunga di "secret" o contiene caratteri non alfabetici...
+            
+            prompt = "Input non valido! Riprova: "; // ...messaggio di errore e salto subito alla chiamata ricorsiva (senza decrementare il numero di tentativi rimasti).
+            
+        } else { // ...altrimenti...
+            
+            let test = check(userInput.toUpperCase(), secret.toUpperCase());
+            
+            console.log(test.output);
+            
+            if (test.guessed) {
+                    
+                console.log("Bravo, hai indovinato!");
+                    
+                attempts = 0; // Impost "attempts" a ultimo tentativo.
+                
+            } else {
+                
+                if (attempts > 0) {
+                    
+                    //console.log("Sbagliato, riprova.");
+                    prompt = "Sbagliato, riprova: "; // Cambio il prompt iniziale.
+                    
+                } else {
+                    
+                    console.log("Hai esaurito i tentativi. Riprova un'altra volta.");
+                    
+                }
+                
+            }
+            
+            attempts--; // Decremento i tentativi rimasti.
+            
         }
-
-        // controlla di aver inserito esattamente wordGameLength caratteri altrimenti dai un errore
-        // if (condition) {
-        //      console.log('\n', chalk.red(`Devi inserire ${wordGameLength} caratteri.`));
-        // }
-
-
-        console.log('\n', `Tentativo ${attempt} di ${max}`);
-        ++attempt;
-
-
-        // if (condition) {
-        //     console.log(chalk.green(' >> HAI VINTO << '));
-        //     return rl.close();
-        // }
-
-        // if (condition) {
-        //     console.log('\n', chalk.red(`Spiacente hai terminato i ${counter} tentativi. La soluzione era ${solution}`), '\n');
-        //     return rl.close();
-        // }
-
-        // Richiamo la funzione fino a esaurimento dei tentativi
-        game(attempt, maxGameAttempt);
+        
+        game(attempts, prompt); // Ricorsione.
+                
     });
+    
 }
 
-game(attempt, maxGameAttempt);
-
-
-
-
-
+game(maxAttempts);
